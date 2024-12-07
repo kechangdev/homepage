3855e8839c692d9daab7e54abf9f9c871ef7e980
Kechang <hanhanso@icloud.com>
2024-12-07 19:42:07 +0800

    optimize style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,109 @@ export default function Home() {
   const cursorX = useMotionValue(0);
   const cursorY = useMotionValue(0);
   const [isVisible, setIsVisible] = useState(true);
-  const [buttonVisible, setButtonVisible] = useState(false); // State for button visibility
+  const [blockSize, setBlockSize] = useState(0); // Size of each service block
+  const servicesContainerRef = useRef<HTMLDivElement | null>(null);
+  const [hasScrolledToMiddle, setHasScrolledToMiddle] = useState(false);
 
-  // Disable right-click context menu
+  const services = [
+    {
+      id: 1,
+      title: "kechang's blog",
+      description: "My musings and ramblings.",
+      image: "https://images.unsplash.com/photo-1677442136019-21780ecad995",
+      link: "https://blog.kechang.uk",
+    },
+    {
+      id: 2,
+      title: "kechang's Alist",
+      description: "Where my files hang out.",
+      image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71",
+      link: "https://pan.kechang.uk",
+    },
+    {
+      id: 3,
+      title: "kechang's Status",
+      description: "How are my servers doing?",
+      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
+      link: "https://status.kechang.uk",
+    },
+    {
+      id: 4,
+      title: "kechang's Qinglong",
+      description: "Automate all the things!",
+      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
+      link: "https://qinglong.kechang.uk",
+    },
+    {
+      id: 5,
+      title: "kechang's Chat",
+      description: "Chat with AI for help and fun!",
+      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
+      link: "https://chat.kechang.uk",
+    },
+    {
+      id: 6,
+      title: "kechang's API",
+      description: "AI-powered API for Kechang's Chat.",
+      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
+      link: "https://api.kechang.uk",
+    },
+    {
+      id: 7,
+      title: "kechang's Bark",
+      description: "Notifications that bark.",
+      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
+      link: "https://bark.kechang.uk",
+    },
+    {
+      id: 8,
+      title: "kechang's YOURLS",
+      description: "Shrink those URLs!",
+      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
+      link: "https://url.kechang.uk/admin",
+    },
+  ];
+
+  const calculateBlockSize = () => {
+    const windowWidth = window.innerWidth;
+    const numberOfServices = services.length;
+    const newSize = Math.min(windowWidth / (numberOfServices + 1), 300); // Max size of 300px
+    setBlockSize(newSize);
+  };
+
   useEffect(() => {
-    const handleContextMenu = (e: MouseEvent) => {
-      e.preventDefault();
+    calculateBlockSize();
+    window.addEventListener('resize', calculateBlockSize);
+    return () => {
+      window.removeEventListener('resize', calculateBlockSize);
     };
-
-    window.addEventListener('contextmenu', handleContextMenu);
-    return () => window.removeEventListener('contextmenu', handleContextMenu);
   }, []);
 
-  // Mouse follower effect
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            setHasScrolledToMiddle(true);
+          }
+        },
+        {
+          root: null,
+          rootMargin: "0px",
+          threshold: 0.5, // Trigger when section is 50% visible
+        }
+    );
+
+    if (servicesContainerRef.current) {
+      observer.observe(servicesContainerRef.current);
+    }
+
+    return () => {
+      if (servicesContainerRef.current) {
+        observer.unobserve(servicesContainerRef.current);
+      }
+    };
+  }, []);
+
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
       setMousePosition({ x: e.clientX, y: e.clientY });
@@ -36,82 +126,19 @@ export default function Home() {
   useEffect(() => {
     cursorX.set(mousePosition.x - 16);
     cursorY.set(mousePosition.y - 16);
-  }, [mousePosition]);
+  }, [mousePosition, cursorX, cursorY]);
 
-  // Handle dynamic show/hide of nav bar based on scroll direction
   useEffect(() => {
     let lastScrollY = window.scrollY;
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
-      if (Math.abs(currentScrollY - lastScrollY) > 5) {
-        setIsVisible(lastScrollY > currentScrollY || currentScrollY < 10);
-        lastScrollY = currentScrollY;
-      }
+      setIsVisible(lastScrollY > currentScrollY || currentScrollY < 10);
+      lastScrollY = currentScrollY;
     };
 
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
-
-  // Sample services data
-  const services = [
-    {
-      id: 1,
-      title: "kechang's blog",
-      description: "My musings and ramblings.",
-      image: "https://images.unsplash.com/photo-1677442136019-21780ecad995",
-      link: "https://blog.kechang.uk"
-    },
-    {
-      id: 2,
-      title: "kechang's Alist",
-      description: "Where my files hang out.",
-      image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71",
-      link: "https://pan.kechang.uk"
-    },
-    {
-      id: 3,
-      title: "kechang's Status",
-      description: "How are my servers doing?",
-      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
-      link: "https://status.kechang.uk"
-    },
-    {
-      id: 4,
-      title: "kechang's Qinglong",
-      description: "Automate all the things!",
-      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
-      link: "https://qinglong.kechang.uk"
-    },
-    {
-      id: 5,
-      title: "kechang's Chat",
-      description: "Chat with AI for help and fun!",
-      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
-      link: "https://chat.kechang.uk"
-    },
-    {
-      id: 6,
-      title: "kechang's API",
-      description: "AI-powered API for Kechang's Chat.",
-      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
-      link: "https://api.kechang.uk"
-    },
-    {
-      id: 7,
-      title: "kechang's Bark",
-      description: "Notifications that bark.",
-      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
-      link: "https://bark.kechang.uk"
-    },
-    {
-      id: 8,
-      title: "kechang's YOURLS",
-      description: "Shrink those URLs!",
-      image: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
-      link: "https://url.kechang.uk/admin"
-    }
-  ];
 
   return (
       <>
@@ -123,11 +150,11 @@ export default function Home() {
         {/* Custom Cursor */}
         <motion.div
             ref={cursorRef}
-            className="fixed w-10 h-10 rounded-full border border-[#bf1b15] pointer-events-none z-50 mix-blend-difference" // Changed cursor edge color to red
+            className="fixed w-10 h-10 rounded-full border border-[#bf1b15] pointer-events-none z-50 mix-blend-difference"
             style={{
               x: cursorX,
               y: cursorY,
-              transition: 'transform 0.05s ease-in-out', // Smoother cursor effect
+              transition: 'transform 0.05s ease-in-out',
               backgroundColor: 'transparent',
               boxShadow: '0 0 10px rgba(255, 255, 255, 0.5)',
             }}
@@ -135,13 +162,13 @@ export default function Home() {
 
         {/* Progress Bar */}
         <motion.div
-            className="fixed top-0 left-0 right-0 h-[2px] bg-[#bf1b15] origin-left z-50" // Changed progress bar color to red
+            className="fixed top-0 left-0 right-0 h-[2px] bg-[#bf1b15] origin-left z-50"
             style={{ scaleX: smoothProgress }}
         />
 
         {/* Navigation */}
         <motion.nav
-            className={`fixed w-full z-40 backdrop-blur-md transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`} // Smooth nav transitions
+            className={`fixed w-full z-40 backdrop-blur-md transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}
         >
           <div className="max-w-[1800px] mx-auto px-8 py-4">
             <div className="flex justify-between items-center">
@@ -163,13 +190,13 @@ export default function Home() {
           <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 0.6 }}
-              transition={{ duration: 1.5, ease: "easeOut" }} // More fluid transitions
+              transition={{ duration: 1.5, ease: "easeOut" }}
               className="absolute inset-0 bg-opacity-70"
           />
           <motion.div
               initial={{ opacity: 0, scale: 1.1 }}
               animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 1.5, delay: 0.5, type: "spring", stiffness: 50, damping: 15 }} // Slightly smoother scalings
+              transition={{ duration: 1.5, delay: 0.5, type: "spring", stiffness: 50, damping: 15 }}
               className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1623039925698-1f96229b1a51?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center"
               style={{ opacity: 0.4 }}
           />
@@ -202,47 +229,6 @@ export default function Home() {
           </motion.div>
         </section>
 
-        {/* Services Section */}
-        <section id="services" className="py-24 px-4 bg-gradient-to-br from-white to-gray-200">
-          <div className="max-w-[1500px] mx-auto">
-            <motion.h2
-                initial={{ opacity: 0, y: 20 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.7, ease: "easeOut" }}
-                viewport={{ once: true }}
-                className="font-['Cormorant_Garamond'] text-4xl md:text-6xl text-center text-black mb-20"
-            >
-              Services
-            </motion.h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-              {services.map((service) => (
-                  <motion.a
-                      href={service.link}
-                      key={service.id}
-                      initial={{ opacity: 0, y: 40 }}
-                      whileInView={{ opacity: 1, y: 0 }}
-                      transition={{ duration: 0.5, delay: service.id * 0.1, ease: "easeOut" }}
-                      viewport={{ once: true }}
-                      className="group relative overflow-hidden cursor-pointer rounded-lg shadow-lg"
-                  >
-                    <div className="absolute inset-0 bg-black/40 z-10 transition-opacity duration-500 group-hover:bg-opacity-0" />
-                    <img
-                        src={service.image}
-                        alt={service.title}
-                        className="object-cover w-full h-[180px] transform transition-transform duration-500 group-hover:scale-105"
-                    />
-                    <div className="absolute inset-0 z-20 p-6 flex flex-col justify-end bg-gradient-to-t from-black via-gray-900 to-transparent transition-opacity duration-500 group-hover:bg-opacity-0">
-                      <h3 className="font-['Cormorant_Garamond'] text-2xl mb-2 text-white">{service.title}</h3> {/* Changed text color to white */}
-                      <p className="text-sm tracking-[0.2em] opacity-0 transform translate-y-4 transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-0 text-white">
-                        {service.description}
-                      </p>
-                    </div>
-                  </motion.a>
-              ))}
-            </div>
-          </div>
-        </section>
-
         {/* About Section */}
         <section id="about" className="py-24 relative overflow-hidden bg-gradient-to-br from-white to-gray-200">
           <motion.div
@@ -266,20 +252,6 @@ export default function Home() {
                 <p className="text-lg leading-relaxed text-black/80 mb-6">
                   A Student!!!
                 </p>
-                <div
-                    className="relative inline-block"
-                    onMouseEnter={() => setButtonVisible(true)}
-                    onMouseLeave={() => setButtonVisible(false)} // Show/hide button on hover
-                >
-                  <a
-                      href="https://github.com/kechangdev"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className={`text-sm tracking-[0.2em] border border-black px-8 py-3 transition-all duration-300 inline-block ${buttonVisible ? 'block' : 'hidden'} bg-black text-white`} // Button visibility and styling
-                  >
-                    VIEW MY WORK
-                  </a>
-                </div>
               </motion.div>
               <motion.div
                   initial={{ opacity: 0, x: 40 }}
@@ -298,6 +270,49 @@ export default function Home() {
           </div>
         </section>
 
+        {/* Services Section */}
+        <section id="services" className="py-24 px-4 bg-gradient-to-br from-white to-gray-200" ref={servicesContainerRef}>
+          <div className="max-w-[1500px] mx-auto">
+            <motion.h2
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7, ease: "easeOut" }}
+                viewport={{ once: true }}
+                className="font-['Cormorant_Garamond'] text-4xl md:text-6xl text-center text-black mb-20"
+            >
+              Services
+            </motion.h2>
+            <div className="relative overflow-hidden">
+              <div className="flex transition-transform duration-300" style={{ transform: `translateX(${hasScrolledToMiddle ? 0 : blockSize + 20}px)` }}>
+                {services.map((service, index) => (
+                    <motion.a
+                        href={service.link}
+                        key={service.id}
+                        initial={{ opacity: 0, x: 40 }}
+                        animate={{ opacity: hasScrolledToMiddle ? 1 : 0, x: hasScrolledToMiddle ? 0 : 40 }}
+                        transition={{ duration: 0.5, ease: "easeOut", delay: index * 0.1 }}
+                        className="group relative overflow-hidden cursor-pointer rounded-lg shadow-lg m-2"
+                        style={{ width: `${blockSize}px`, height: '200px' }}
+                    >
+                      <div className="absolute inset-0 bg-black/40 z-10 transition-opacity duration-500 group-hover:bg-opacity-0" />
+                      <img
+                          src={service.image}
+                          alt={service.title}
+                          className="object-cover w-full h-full transform transition-transform duration-500 group-hover:scale-105"
+                      />
+                      <div className="absolute inset-0 z-20 p-6 flex flex-col justify-end bg-gradient-to-t from-black via-gray-900 to-transparent transition-opacity duration-500 group-hover:bg-opacity-0">
+                        <h3 className="font-['Cormorant_Garamond'] text-xl mb-2 text-white">{service.title}</h3>
+                        <p className="text-sm tracking-[0.2em] opacity-0 transform translate-y-4 transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-0 text-white">
+                          {service.description}
+                        </p>
+                      </div>
+                    </motion.a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+
         {/* Footer */}
         <footer id="contact" className="py-24 bg-gradient-to-br from-white to-gray-200">
           <div className="max-w-[1500px] mx-auto px-4">
@@ -308,7 +323,7 @@ export default function Home() {
                 <a href="https://github.com/kechangdev" target="_blank" rel="noopener noreferrer" className="hover:text-[#bf1b15] transition-colors duration-300">GITHUB</a>
               </div>
             </div>
-            <div className="border-t border-black pt-4"> {/* Changed border color to black */}
+            <div className="border-t border-black pt-4">
               <p className="text-center text-sm text-black">
                 © {new Date().getFullYear()} KECHANG. ALL RIGHTS RESERVED
               </p>
@@ -318,7 +333,7 @@ export default function Home() {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-black" fill="none" viewBox="0 0 24 24"
                        stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                          d="M12 3c-4.97 0-9 4.03-9 9a9 9 0 0016.26 5.32c.45-.73.74-1.54.74-2.32 0-2.22-2.02-4-4.5-4h-1.5V9h1.5c2.48 0 4.5 1.78 4.5 4a4.5 4.5 0 01-4.5 4.5H12v-3h2.5a1.5 1.5 0 000-3H12V9h1.5C17.67 9 21 12.33 21 16.5c0 1.78-.61 3.42-1.62 4.73A9 9 0 0012 21z"/>
+                          d="M12 3c-4.97 0-9 4.03-9 9a9 9 0 0016.26 5.32c.45-.73.74-1.54.74-2.32 0-2.22-2.02-4-4.5-4h-1.5V9h1.5c2.48 0 4.5 1.78 4.5 4a4.5 4.5 0 01-4.5 4.5H12v-3h2.5a1.5 1.5 0 000-3H12V9h1.5C17.67 9 21 12.33 21 16.5c0 1.78-.61 3.42-1.62 4.73A9 9 0 0012 21z" />
                   </svg>
                   <span>View Project</span>
                 </a>
